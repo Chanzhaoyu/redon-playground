06a8046b0d26334b143d49898d5f405087c77dab
ChenZhaoYu <790348264@qq.com>
2023-11-24 11:56:53 +0800

    feat: Columns

--- app/(playground)/tailwindcss/page.tsx
+++ app/(playground)/tailwindcss/page.tsx
@@ -1,5 +1,63 @@
 const Page = () => {
-  return <div>Tailwindcss Playground</div>;
+  return (
+    <div className="p-4">
+      <div className="m-auto container space-y-4">
+        <div>
+          <h2 className="mb-4 font-semibold">Columns</h2>
+          <div className="columns-4 gap-4 space-y-4">
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/5uVR7Uixv2C56ZUtabpvkAPzgQ.webp"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/QRdJkeGZvKgkFlbJyJHCujLgzSc.webp"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/kpckiL3HTUTdZUFlqqME53gL8OE.webp"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/iirsIm6zsea6dQLtCqcESc1S4.webp"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/eb2BZjgsz3bVf7mWNmpzEvPzWg.webp"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/JOy0vG5gU10zyvo6FKKiKmY.webp"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/ogJ1ApXfw4yX5O5oqXdG9V1WHFo.webp?scale-down-to=4096"
+                alt=""
+              />
+            </div>
+            <div className="overflow-hidden rounded-md">
+              <img
+                src="https://framerusercontent.com/images/64n4wnVJKDJspLlFZ6DarCP0M4.jpg"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
